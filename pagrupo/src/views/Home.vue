5caf177139a82ebb1f81a4c0289dea322619de94
gsmile-star <anag201917@gmail.com>
2020-11-12 18:56:45 -0500

    Vista de features #11

---
+++ pagrupo/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">       			
+    <Botonera msg="It's all in one place"/>   
+
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Botonera from '@/components/Botonera.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Botonera
+  }
+}
+</script>
